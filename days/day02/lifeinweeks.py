e615587dd0f9c0c816db28b04d567e72c3b8d006
Mr. Erlison <sisal.cordame_0a@icloud.com>
2022-12-11 19:08:22 -0300

    Fix needed

--- days/day02/lifeinweeks.py
+++ days/day02/lifeinweeks.py
@@ -1,9 +1,15 @@
 age = input("What is your current age? ")
+
+# o que é 90?
 days = 90 * 365
 weeks = 90 * 52
 months = 90 * 12
+
 age = int(age)
+
+# o que é dleft wleft e mleft?
 dleft = days - age * 365
 wleft = weeks - age * 52
 mleft = months - age * 12
+
 print(f'You have {dleft} days, {wleft} weeks, and {mleft} months left.') 
