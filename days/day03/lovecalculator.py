15f88b705db5861b96e01c09cac427d1888f4a3b
wvbwagner <wvbwagner@gmail.com>
2022-12-13 21:42:16 -0300

    Update day 03

--- days/day03/lovecalculator.py
+++ days/day03/lovecalculator.py
@@ -1,15 +1,16 @@
-name1 = input('What is your name? \n').upper()
-name2 = input('What is their name? \n').upper()
+name1 = input('What is your name? \n')
+name2 = input('What is their name? \n')
 cont = 0
 word = 'TRUE'
+names = (name1 + name2).upper()
 for i in word:
-    cont += name1.count(i) + name2.count(i)
+    cont += names.count(i)
 total = str(cont)
 cont = 0
-word = "LOVE"
+word = 'LOVE'
 for i in word:
-    cont += name1.count(i) + name2.count(i)
-total  += str(cont)
+    cont += names.count(i)
+total += str(cont)
 total = int(total)
 if total < 10 or total > 90:
     print(f'Your score is {total}, you go together like coke and mentos.')
