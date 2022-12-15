9641034cd1da5bcfc459277622f3ae705413f05e
wvbwagner <wvbwagner@gmail.com>
2022-12-15 05:36:59 -0300

    Adcionando Hurdle 1.py, fase 1 de Reeborg

---
+++ days/day06/Hurdle 1.py
@@ -0,0 +1,20 @@
+def jumpOver():
+    move()
+    turn_left()
+    move()
+    turn_left()
+    turn_left()
+    turn_left()
+    move()
+    turn_left()
+    turn_left()
+    turn_left()
+    move()
+    turn_left()
+while not at_goal():
+    jumpOver()
+
+################################################################
+# WARNING: Do not change this comment.
+# Library Code is below.
+################################################################
