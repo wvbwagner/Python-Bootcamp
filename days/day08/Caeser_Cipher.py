3d3eb2e3990f7f3288c9e97e487321675dde29a7
wvbwagner <wvbwagner@gmail.com>
2022-12-21 15:42:59 -0300

    Alterada entrada do usuário

--- days/day08/Caeser_Cipher.py
+++ days/day08/Caeser_Cipher.py
@@ -1,17 +1,8 @@
 import my_cripto
+import teste
 
 codeOrDecode = input('Do you want to code or decode the message? ').lower()
 
-if codeOrDecode == 'code':
-    text = input('Enter the text you want to encrypt: ').lower()
-    jumps = int(input('Enter the number of shifts: '))
-    cipher = my_cripto.criptografar(text, jumps)
-    print(f'Crypto message is: {cipher}')
-    
-elif codeOrDecode == 'decode':
-    text = input('Enter the text you want to decript: ').lower()
-    key = int(input('Enter the key to decript: '))
-    clearText = my_cripto.decodificar(text, key)
-    print(f'Decripted message is: {clearText}')
-
+mensagem = teste.entradaUsuario(codeOrDecode)
+print(mensagem)
 
