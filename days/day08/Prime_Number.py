2f8f29cd793d1ea73b3f610de27bad882e5b5e65
wvbwagner <wvbwagner@gmail.com>
2022-12-21 07:25:25 -0300

    Alterado Prime_Number.py

--- days/day08/Prime_Number.py
+++ days/day08/Prime_Number.py
@@ -1,13 +1,12 @@
 def prime_checker(number):
     divisor = 0
-    loopEnd = number + 1
-    for i in range(1, loopEnd):
+    for i in range(2, number):
         if number % i == 0:
             divisor += 1
-    if divisor == 2:
-        print(f'It\'s a prime number.')
+    if divisor == 0:
+        print('It\'s a prime number.')
     else:
-        print(f'It\'s not a prime number.')
+        print('It\'s not a prime number.')
         
 n = int(input("Check this number: "))
 prime_checker(number=n)
