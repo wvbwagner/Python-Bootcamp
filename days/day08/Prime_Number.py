99d1022a634e6d6319d257ca456a3dba7667c0d2
wvbwagner <wvbwagner@gmail.com>
2022-12-21 15:41:14 -0300

    Colocado verificação de divisor dento do for

--- days/day08/Prime_Number.py
+++ days/day08/Prime_Number.py
@@ -3,11 +3,10 @@ def prime_checker(number):
     for i in range(2, number):
         if number % i == 0:
             divisor += 1
-    if divisor == 0:
-        print('It\'s a prime number.')
-    else:
-        print('It\'s not a prime number.')
-        
+            print('It\'s not a prime number.')
+            exit(0)
+    print('It\'s a prime number.')
+     
 n = int(input("Check this number: "))
 prime_checker(number=n)
 
