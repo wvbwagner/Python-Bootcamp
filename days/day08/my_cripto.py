ce579861f024031b8cbb44e9c37fcd780c04842a
wvbwagner <wvbwagner@gmail.com>
2022-12-22 07:33:45 -0300

    Usando alfabeto completo devido a limitações no uso de chr e ord

--- days/day08/my_cripto.py
+++ days/day08/my_cripto.py
@@ -1,26 +1,38 @@
+import string
+
+alpha = string.printable
+size = len(alpha)
+
 def entradaUsuario(choice):
     if choice == 'code':    
         text = input('Enter the text you want to encrypt: ')
         jumps = int(input('Enter the number of shifts: '))
-        return codificador(text, jumps)
+        return criptografar(text, jumps)
 
     elif choice == 'decode':
         text = input('Enter the text you want to decript: ')
         key = int(input('Enter the key to decript: '))
-        return decodificador(text, key)
+        return decodificar(text, key)
 
     else:
         return "Invalid entry"
         exit(1)
 
-def codificador(texto, passo):
-    codigo = ''
-    for letra in texto:
-        codigo += chr((ord(letra) + passo))
-    return codigo
-
-def decodificador(cipher, passo):
+def criptografar(texto, passo):
+    cipher = ''
+    passoReal = size - passo
+    for letter in texto:
+        if letter not in alpha:
+            cipher += letter
+        else:
+            cipher += (alpha[alpha.index(letter) - passoReal])
+    return str(cipher)
+    
+def decodificar(texto, passo):
     clear = ''
-    for letra in cipher:
-        clear += chr((ord(letra) - passo))
-    return clear
+    for letter in texto:
+        if letter not in alpha:
+            clear += letter
+        else:
+            clear += (alpha[alpha.index(letter) - passo])
+    return str(clear)
