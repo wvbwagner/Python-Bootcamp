c1a67f1cc377dd4943188a89bedd1422b8de6286
wvbwagner <wvbwagner@gmail.com>
2022-12-22 10:38:30 -0300

    Adicionando v1 de Gradind_Program.py

---
+++ days/day09/Grading_Program.py
@@ -0,0 +1,26 @@
+student_scores = {
+  "Harry": 81,
+  "Ron": 78,
+  "Hermione": 99, 
+  "Draco": 74,
+  "Neville": 62,
+}
+student_grades = {}
+
+'''def checkGrades(value):
+    if value <= 70:
+        grade = 'Fail'
+    elif value <= 80:
+        grade = 'Acceptable'
+    elif value <= 90:
+        grade = 'Exceeds Expectations'
+    else:
+        grade = 'Outstanding'
+    return grade
+
+for names, grades in student_scores.items():
+    student_grades[names] = checkGrades(grades)'''
+for grades in student_scores.values():
+    x = student_scores[grades]
+    print(x)
+print(student_grades)
