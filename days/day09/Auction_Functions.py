eaa72a4dd754dcae142ed3bfde977baba7c5123b
wvbwagner <wvbwagner@gmail.com>
2022-12-24 07:45:46 -0300

    Adicionando Auction_Functions.py, contendo as funções para SecretAuction.py

---
+++ days/day09/Auction_Functions.py
@@ -0,0 +1,28 @@
+import replit
+
+def secretAuction():
+    auction = []
+    buyers = {}
+    while True:
+        replit.clear()
+        name = input('What\'s your name? ')
+        bid = input('What\'s your bid? $')
+        buyers['Name'] = name
+        buyers['Bid'] = bid
+        auction.append(buyers.copy())
+        question = input('Are there any other bidders? Type "yes" or "no": ')
+        if question == 'no':
+            break
+    return auction
+
+def checkWinner(rank):
+    winner = []
+    max = 0
+    for i in rank:
+        bid = int(i.get('Bid'))
+        if max < bid:
+            max = bid
+            winner.clear()
+            winner.append(i.get('Name'))
+            winner.append(i.get('Bid'))
+    return winner
