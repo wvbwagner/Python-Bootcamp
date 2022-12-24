62f6a16629a01e006491ca35054329aced9ed884
wvbwagner <wvbwagner@gmail.com>
2022-12-24 07:57:15 -0300

    v2 de SecretAuction.py

--- days/day09/SecretAuction.py
+++ days/day09/SecretAuction.py
@@ -1,5 +1,4 @@
 import Auction_Functions
 
 auction = Auction_Functions.secretAuction()
-winner = Auction_Functions.checkWinner(auction)
-print(f'Winner is {winner[0]} with a bid of ${winner[1]}')
+print(Auction_Functions.checkWinner(auction))
