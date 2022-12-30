2eed1d205c79e3eb823efe849ad87d8e573c6d00
wvbwagner <wvbwagner@gmail.com>
2022-12-30 08:34:51 -0300

    Funções para Blackjack Capstone

---
+++ days/day11/Blackjack_Functions.py
@@ -0,0 +1,75 @@
+from random import choice
+from replit import clear
+
+'''
+Regras:
+Acima de 21, perde
+Abaixo de 17, obrigatório outra carta
+A: vale 1 ou 11, vc escolhe
+J, Q, K: valem 10
+'''
+cards = [2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11]
+
+def dealCards():
+    return choice(cards)
+
+def myCards():
+    myCards = []
+    for i in range(2):
+        myCards.append(dealCards())
+    return myCards
+
+def cpuCards():
+    global cpuPoints
+    cpuCards = []
+    cpuPoints = sum(cpuCards)
+    while cpuPoints <  17:
+        cpuCards.append(dealCards())
+        cpuPoints = sum(cpuCards)
+    return cpuCards
+
+myHand = myCards()
+myPoints = sum(myHand)
+cpuHand = cpuCards()
+
+def startGame():
+    global myPoints
+    while True:
+        print(f'Your cards are {myHand}. Your score is {myPoints}')
+        print(f'Computer\'s first card is {cpuHand[0]}')
+        question = input("Type 'y' to get another card, type 'n' to pass: ")
+        if question == 'y':
+            myHand.append(dealCards())
+            myPoints += myHand[-1]
+            if myPoints > 21:
+                break
+        else:
+            break
+    checkResults()
+
+def checkResults():
+    print(f'Your final hand is {myHand}, final score: {myPoints}')
+    print(f'Computer\'s final hand is {cpuHand}, final score: {cpuPoints}')
+    if myPoints > 21:
+        print('You went over. You lose!')
+    elif cpuPoints > myPoints:
+        print('You lose!')
+    elif cpuPoints > 21 and myPoints < 21:
+        print('CPU went over. You win!')
+    elif cpuPoints < myPoints:
+        print('You win!')
+    elif cpuPoints == myPoints:
+        print('It\'s a draw')
+
+def wannaPlay():
+    play = True
+    while play:
+        question = input('Do you want to play BlackJack Capstone: [y] [n] ')
+        if question == 'y':
+            clear()
+            startGame()
+        elif question == 'n':
+            print('Good bye!')
+            exit(0)
+        else:
+            print('Invalid answer')
