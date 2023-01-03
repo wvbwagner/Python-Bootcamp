0a751c1263793788ec96c462268bbdc53322214d
wvbwagner <wvbwagner@gmail.com>
2023-01-03 10:15:57 -0300

    Alteradas as funçoes initialPlay, comparePlayers, presentPlayers e play

--- days/day14/Higher_Lower_Function.py
+++ days/day14/Higher_Lower_Function.py
@@ -3,20 +3,17 @@ from random import choice
 from logo import vs, logo
 from replit import clear
 
-score = 0
 storePlayer = {}
 def initialPlay(score):
     global storePlayer
     if score == 0:
         player1 = choice(data)
-        player2 = choice(data)
-        if player2 == player1:
-            player2 = choice(data)
     else:
         player1 = storePlayer
+
+    player2 = choice(data)
+    if player2 == player1:
         player2 = choice(data)
-        if player2 == player1:
-            player2 = choice(data)
     storePlayer = player2
     return player1, player2
 
@@ -27,27 +24,29 @@ def presentPlayers(score):
     print(f'Compare B: {secondPlayer.get("name")}, a {secondPlayer.get("description")} from {secondPlayer.get("country")}')
     playerOne = firstPlayer.get('follower_count')
     playerTwo = secondPlayer.get('follower_count')
-    if playerOne > playerTwo:
-        max = playerOne
-    else:
-        max = playerTwo
-    return playerOne, playerTwo, max
+    
+    return playerOne, playerTwo
 
-def comparePlayers(valuesToCompare):
-    global score
-    playerOne, playerTwo, maximum = valuesToCompare
+def comparePlayers(valuesToCompare, score):
+    playerOne, playerTwo = valuesToCompare
+    if playerOne > playerTwo:
+        maximum = playerOne
+    else:
+        maximum = playerTwo
     question = input('Who has more followers? Type "A" or "B": ').strip().upper()
     if question == 'A' and maximum == playerOne or question == 'B' and maximum == playerTwo:
         clear()
         score += 1
         print(logo)
         print(f'You\'re right, current score: {score}.')
+        return score
     else:
         clear()
         print(f'Sorry, that\'s wrong! Final score: {score}.')
         exit(1)
 
 def play():
+    score = 0
     print(logo)
     while True:
-        comparePlayers(presentPlayers(score))
+        score = comparePlayers(presentPlayers(score), score)
