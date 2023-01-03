654d0375414b9ccbd1b1573ab9a217716494885c
wvbwagner <wvbwagner@gmail.com>
2023-01-02 21:12:18 -0300

    Alteradas funções para execução do jogo

--- days/day14/Higher_Lower_Function.py
+++ days/day14/Higher_Lower_Function.py
@@ -3,34 +3,55 @@ from random import choice
 from logo import vs, logo
 from replit import clear
 
-def game():
-    score = 0
-    firstPlayer = choice(data)
-    secondPlayer = choice(data)
-    if secondPlayer == firstPlayer:
-        secondPlayer = choice(data)
+score = 0
+storePlayer = {}
+def initialPlay(score):
+    global storePlayer
+    if score == 0:
+        player1 = choice(data)
+        player2 = choice(data)
+        if player2 == player1:
+            player2 = choice(data)
+    else:
+        player1 = storePlayer
+        player2 = choice(data)
+        if player2 == player1:
+            player2 = choice(data)
+    storePlayer = player2
+    return player1, player2
+
+def presentPlayers(score):
+    
+    firstPlayer, secondPlayer = initialPlay(score)
+    #secondPlayer = players[1]
     print(logo)
+    print(f'Compare A: {firstPlayer.get("name")}, a {firstPlayer.get("description")} from {firstPlayer.get("country")}')
+    print(vs)
+    print(f'Compare B: {secondPlayer.get("name")}, a {secondPlayer.get("description")} from {secondPlayer.get("country")}')
+    playerOne = firstPlayer.get('follower_count')
+    playerTwo = secondPlayer.get('follower_count')
+    if playerOne > playerTwo:
+        max = playerOne
+    else:
+        max = playerTwo
+    return playerOne, playerTwo, max
+
+def comparePlayers(valuesToCompare):
+    global score
+    playerOne = valuesToCompare[0]
+    playerTwo = valuesToCompare[1]
+    maximum = valuesToCompare[2]
+    question = input('Who has more followers? Type "A" or "B": ').strip().upper()
+    if question == 'A' and maximum == playerOne or question == 'B' and maximum == playerTwo:
+        clear()
+        score += 1
+        print(logo)
+        print(f'You\'re right, current score: {score}.')
+    else:
+        clear()
+        print(f'Sorry, that\'s wrong! Final score: {score}.')
+        exit(1)
+
+def play():
     while True:
-        print(f'Compare A: {firstPlayer.get("name")}, a {firstPlayer.get("description")} from {firstPlayer.get("country")}')
-        print(vs)
-        print(f'Compare B: {secondPlayer.get("name")}, a {secondPlayer.get("description")} from {secondPlayer.get("country")}')
-        A = firstPlayer.get('follower_count')
-        B = secondPlayer.get('follower_count')
-        if A > B:
-            max = A
-        else:
-            max = B
-        question = input('Who has more followers? Type "A" or "B": ').strip().upper()
-        if question == 'A' and max == A or question == 'B' and max == B:
-            clear()
-            score += 1
-            print(logo)
-            print(f'You\'re right, current score: {score}.')
-        else:
-            clear()
-            print(f'Sorry, that\'s wrong! Final score: {score}.')
-            exit(1)
-        firstPlayer = secondPlayer
-        secondPlayer = choice(data)
-        if secondPlayer == firstPlayer:
-            secondPlayer = choice(data)
+        comparePlayers(presentPlayers(score))
